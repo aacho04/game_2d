7e7c3d1daefcfb5314b3cb03774f04a532c318f3
aacho04 <aaditinarayan14@gmail.com>
2026-01-22 15:18:11 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ package game_2d;
 public class Main {
     public static void main(String[] args) {
         
+        JFrame window=new JFrame("2D Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false); 
+        
     }
 }
